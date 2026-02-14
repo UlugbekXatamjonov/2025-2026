0047bd77cc5e74e1e243c0a7844a37b987390122
UlugbekXatamjonov <xatamjonovulugbek17@gmail.com>
2026-02-14 11:49:31 +0500

    26-02-14

--- 8_Green/django_darslari/takrorlash/mainapp/admin.py
+++ 8_Green/django_darslari/takrorlash/mainapp/admin.py
@@ -8,4 +8,6 @@ from .models import New
 @admin.register(New)
 class New_Admin(admin.ModelAdmin):
     list_display = ('title', 'author', 'tag', 'status', 'created_on')
+    list_filter = ('author', 'tag', 'status', 'created_on') # kam uchraydigan ma'umotlar
+    search_fields = ('title', 'body')# xar-xil bo'ladigan ma'umotlar
 
--- 8_Green/django_darslari/takrorlash/mainapp/models.py
+++ 8_Green/django_darslari/takrorlash/mainapp/models.py
@@ -3,11 +3,17 @@ from django.db import models
 # Create your models here.
 
 class New(models.Model):
-    """  """
-    title = models.CharField(max_length=250)
+    """
+    default = ""    -> standart qiymat
+    null = True     -> Maydonni bo'sh qoldiradi 
+    blank = True    -> Maydonni bo'sh qoldiradi
+    unique = True   -> Shu maydonni yagona 
+    
+    """
+    title = models.CharField(max_length=250, unique=True)
     body = models.TextField()
-    photo = models.ImageField(upload_to='photos/')
-    video = models.FileField(upload_to='videos/')
+    photo = models.ImageField(upload_to='photos/', blank=True, null=True)
+    video = models.FileField(upload_to='videos/', blank=True, null=True)
     tag = models.CharField(max_length=100)
     views_count = models.PositiveIntegerField(default=1)
     author = models.CharField(max_length=50, default="Sobirjonova Z.")
@@ -16,8 +22,19 @@ class New(models.Model):
     status = models.BooleanField(default=True) # True / False
     created_on = models.DateTimeField(auto_now_add=True)
 
-    # def __str__(self):
-    #     return f"{self.title} - {self.author}"
+    def __str__(self):
+        return f"{self.title} - {self.author}"
+
+
+
+
+
+
+
+
+
+
+
 
 
 
