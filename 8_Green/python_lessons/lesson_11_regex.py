0fb6610fe2dad353b6430b9186cc1626927714d0
UlugbekXatamjonov <xatamjonovulugbek17@gmail.com>
2026-04-20 14:57:33 +0500

    26-04-20

--- 8_Green/python_lessons/lesson_11_regex.py
+++ 8_Green/python_lessons/lesson_11_regex.py
@@ -17,16 +17,19 @@ search()    -   Agar satrning istalgan joyida moslik mavjud bo'lsa, Match ob'ekt
 """
 from re import match, findall, search
 
-regex1 = r"^[\d]{4}$"
-savol1 = input("4 honalik raqam kiriting: ")
+# regex1 = r"^[\d]{4}$"
+# savol1 = input("4 honalik raqam kiriting: ")
 
-if match(regex1, savol1):
-    print(f"Tog'ri ")
-else:
-    print(f"Notog'ri ")
+# if match(regex1, savol1):
+#     print(f"Tog'ri ")
+# else:
+#     print(f"Notog'ri ")
 
+phone = input("Telefon raqami kiriting: ")
+pattern = r"^[+]998[\d]{2}$"
+print(match(pattern, phone))
 
-
+""" +998(99) 325-74-74 """
 
 
 
