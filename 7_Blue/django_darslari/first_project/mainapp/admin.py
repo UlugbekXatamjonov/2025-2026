6309706e9613a7941fafd9d080c6d6f884bdf720
UlugbekXatamjonov <xatamjonovulugbek17@gmail.com>
2026-06-01 13:34:55 +0500

    add

--- 7_Blue/django_darslari/first_project/mainapp/admin.py
+++ 7_Blue/django_darslari/first_project/mainapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import News
+
 
 # Register your models here.
+@admin.register(News)
+class News_Admin(admin.ModelAdmin):
+    list_display = ("title", 'author')
